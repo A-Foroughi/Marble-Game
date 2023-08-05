76a2cf192534b22fed4bd054fe334be880fed71c
Amir <amir.foroughi85@gmail.com>
2023-08-05 10:03:05 +0330

    Some additionals added

--- Marble Game.py
+++ Marble Game.py
@@ -1,9 +1,11 @@
 print("There are some marbles on the table and we take 1 or 2 each turn.")
 print("Who takes the last wins.")
 n = int(input("How many marbles should we start with? "))
+t = 0
 if n % 3 == 0:
     # Play second
     print("I play second. You start.")
+    t = 1
 else:
     # Play first
     print("I play first.")
@@ -11,7 +13,8 @@ else:
     n = n - m
     print(f"I take {m}.")
 while n > 0:
-    print(f"There is {n} marbles left.")
+    if t == 0:
+        print(f"There is {n} marbles left.")
     m = int(input("It's your turn. How many you take? "))
     if m < 1 or m > 2:
         print(f"I got you. I said take 1 or 2, not {m}.")
@@ -21,5 +24,5 @@ while n > 0:
     m = n % 3
     n = n - m
     print(f"I take {m}.")
-
+    t = 0
 print("I won. Easy")    
